de3de9d3e4f6bb035c876a5a77c5d1c9420141c4
sun1024 <sunnanbin@gmail.com>
2019-03-14 21:34:29 +0800

    'Updated'

---
+++ push.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+os.system('hexo d \-g')
+os.system(ur'git add .')
+os.system("git commit -m 'Updated'")
+os.system('git push origin hexo')
+print 'Over!'
